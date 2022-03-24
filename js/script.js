0ee1484c5ab7bdd49eb51a5b56c4b6d9d49448de
Calistamart <calista.martinez@holis-consulting.com>
2022-03-24 17:30:46 +0100

    enregistrement des recettes + amelioration de l'ui

--- js/script.js
+++ js/script.js
@@ -1,25 +1,41 @@
-// function addFields()
-// Fonction pour rajouter des champs {
-//   // Generate a dynamic number of inputs
-//   var number = document.getElementById("ingredientsForm").value;
-//   // Get the element where the inputs will be added to
-//   var container = document.getElementById("container");
-//   // Remove every children it had before
-//   while (container.hasChildNodes()) {
-//     container.removeChild(container.lastChild);
-//   }
-//   for (i = 0; i < number; i++) {
-//     // Append a node with a random text
-//     container.appendChild(document.createTextNode("Ingredient " + (i + 1)));
-//     // Create an <input> element, set its type and name attributes
-//     var input = document.createElement("input");
-//     input.type = "text";
-//     input.name = "ingredientsForm" + i;
-//     container.appendChild(input);
-//     // Append a line break
-//     container.appendChild(document.createElement("br"));
-//   }
-// }
+// Enregistre les recettes avec localStorage
+var recettes = JSON.parse(localStorage.getItem('recettes')) || []
+const results = document.querySelector(".results pre");
+
+results.innerText = ''
+recettes.forEach((recette)=>results.innerText += JSON.stringify(recette))
+
+let addFieldBtn = document.querySelector('#addField')
+addFieldBtn.onclick = addField
+
+let recetteForm = document.querySelector('form#recetteForm')
+recetteForm.onsubmit = handleFormSubmit
+
+// Fonction pour rajouter des champs (pour les ingrédients)
+function addField(e) {
+  e.preventDefault()
+  e.stopPropagation()
+  var input = document.createElement("input");
+  input.type = "text";
+  input.name = "ingredientsForm";
+  container.appendChild(input);
+  container.appendChild(document.createElement("br"));
+}
+
+// Affiche un JSON à partir des données du formulaire.
+function handleFormSubmit(event) {
+  event.preventDefault();
+
+  const data = new FormData(this);
+
+  const formJSON = Object.fromEntries(data.entries());
+
+  recettes.push(formJSON)
+
+  results.innerText = ''
+  recettes.forEach((recette)=>results.innerText += JSON.stringify(recette))
+  localStorage.setItem('recettes', JSON.stringify(recettes))
+}
 
 class Recipe {
   constructor(name, ingredients, ingredientsQuantity, cookingTime, steps) {
@@ -41,55 +57,12 @@ let couscous = new Recipe(
 
 console.log(couscous);
 
-function handleFormSubmit(event) {
-  event.preventDefault();
+// Assigner le resultat de la recette dans une variable
+// let savedRecipe = handleFormSubmit();
 
-  const data = new FormData(event.target);
-
-  const formJSON = Object.fromEntries(data.entries());
-
-  const results = document.querySelector(".results pre");
-  results.innerText = JSON.stringify(formJSON, null, 2);
-}
-
-const form = document.querySelector(".contact-form");
-form.addEventListener("submit", handleFormSubmit);
-
-// input.addEventListener("input", function (event) {
-//   output.innerText = event.target.value;
-// });
-
-// let ingredients = ["Voilz", "iold"];
-
-// ingredients.push("visl");
-
-// console.log(ingredients);
-
-// document.getElementById("listIngredients");
-
-function sendData(data) {
-  let XHR = new XMLHttpRequest();
-  let urlEncodeData = "";
-  let urlEncodeDataPairs = [];
-  let name;
-
-  for (name in data) {
-    urlEncodeDataPairs.push(
-      encodeURIComponent(name) + "=" + encodeURIComponent(data[name])
-    );
-  }
-
-  urlEncodeData = urlEncodeDataPairs.join("&").replace(/%20/g, "+");
-
-  XHR.addEventListener("load", function (event) {
-    alert("Ouais ! Données envoyées et réponse chargée.");
-  });
-
-  XHR.addEventListener("error", function (event) {
-    alert("Oups ! Quelque chose s'est mal passé.");
-  });
-
-  XHR.open('POST','https://');
-
-  XHR.send(FD);
-}
+// Faire un truc avec des arrays pour les ingrédients et les quantités, et les associer
+function ListeDIngredients () {
+  let ingredients = [];
+  ingredients.push(document.getElementById("champ-liste-des-ingredients".value));
+  console.log(ingredients);
+};
