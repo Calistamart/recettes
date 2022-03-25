e6ed19f26300937a29395de51e9592cba8287cb8
Calistamart <calista.martinez@holis-consulting.com>
2022-03-25 17:26:33 +0100

    youhou

--- js/script.js
+++ js/script.js
@@ -1,26 +1,37 @@
-// Enregistre les recettes avec localStorage
-var recettes = JSON.parse(localStorage.getItem('recettes')) || []
-const results = document.querySelector(".results pre");
-
-results.innerText = ''
-recettes.forEach((recette)=>results.innerText += JSON.stringify(recette))
-
-let addFieldBtn = document.querySelector('#addField')
-addFieldBtn.onclick = addField
-
-let recetteForm = document.querySelector('form#recetteForm')
-recetteForm.onsubmit = handleFormSubmit
-
 // Fonction pour rajouter des champs (pour les ingrédients)
 function addField(e) {
-  e.preventDefault()
-  e.stopPropagation()
+  e.preventDefault();
+  e.stopPropagation();
   var input = document.createElement("input");
   input.type = "text";
-  input.name = "ingredientsForm";
+  input.name = "ingredients[]";
   container.appendChild(input);
   container.appendChild(document.createElement("br"));
 }
+
+let addFieldBtn = document.querySelector("#addField");
+addFieldBtn.onclick = addField;
+
+// Le best code recupère le formulaire, en fait un objet et l'enregistre avec localStorage
+let allRecipes = [];
+
+function addRecipe(e) {
+  e.preventDefault
+  let recipe = {
+    id: Date.now(),
+    nom: document.getElementById("nom-de-la-recette").value,
+    cookingTime: document.getElementById("temps-de-cuisson").value,
+    steps: separateSteps()
+  }
+  console.log(recipe)
+
+  allRecipes.push(recipe);
+
+  localStorage.setItem('MonLivreDeRecettes', JSON.stringify(allRecipes));
+}
+
+let test = document.querySelector("#test-button");
+test.onclick = addRecipe;
 
 // Affiche un JSON à partir des données du formulaire.
 function handleFormSubmit(event) {
@@ -30,18 +41,94 @@ function handleFormSubmit(event) {
 
   const formJSON = Object.fromEntries(data.entries());
 
-  recettes.push(formJSON)
+  recettes.push(formJSON);
 
-  results.innerText = ''
-  recettes.forEach((recette)=>results.innerText += JSON.stringify(recette))
-  localStorage.setItem('recettes', JSON.stringify(recettes))
+  results.innerText = "";
+  recettes.forEach((recette) => (results.innerText += JSON.stringify(recette)));
+  localStorage.setItem("recettes", JSON.stringify(recettes));
 }
 
+// Enregistre la recette avec localStorage
+var recettes = JSON.parse(localStorage.getItem("recettes")) || [];
+const results = document.querySelector(".results pre");
+
+results.innerText = "";
+recettes.forEach((recette) => (results.innerText += JSON.stringify(recette)));
+
+let recetteForm = document.querySelector("form#recetteForm");
+recetteForm.onsubmit = handleFormSubmit;
+
+// Faire un array pour les ingrédients
+
+
+function ingredientsListArray() {
+  let inputs = document.getElementsByName("ingredients[]");
+  let tabIngredients = [];
+  let phraseIngredients = "Les ingrédients sont : ";
+
+  const renderer = document.getElementById("ingr")
+  renderer.innerHTML = ''
+
+  inputs.forEach((input, i)=>{
+    renderer.innerHTML += "ingredient[" + i + "] = " + input.value + " <br> ";
+    tabIngredients.push(input.value)
+  })
+};
+
+let ingredientsArrayButton = document.getElementById("ingredientsArrayButton");
+ingredientsArrayButton.onclick = ingredientsListArray;
+
+// Bouton + pour afficher la saisie de recette
+function ShowRecipeSaisie() {
+  let div = document.getElementById("saisie-de-la-recette");
+  if (div.style.display === "none") {
+    div.style.display = "block";
+  }
+  else {
+    div.style.display = "none";
+  }
+};
+
+let showRecipeSaisieButton = document.getElementById("bouton-plus");
+showRecipeSaisieButton.onclick = ShowRecipeSaisie;
+
+// Séparer chaque étape dans un array (par un ;)
+function separateSteps() {
+  const recipe = document.getElementById("etapes-de-la-recette").value;
+  const steps = recipe.split(";");
+  console.log(steps);
+  return steps;
+}
+
+let separateStepsButton = document.getElementById("save-button");
+separateStepsButton.onclick = separateSteps;
+
+// Bouton pour supprimer toutes les recettes
+let deleteAllButton = document.querySelector("#delete-all-button");
+deleteAllButton.onclick = deleteButton;
+
+function deleteButton(event) {
+  event.preventDefault();
+  localStorage.clear();
+  location.reload();
+}
+
+
+
+
+
+// Faire que le array des ingrédient rentre dans l'objet recette
+// Supprimer qu'une seule recette
+// Modifier les recettes
+
+
+
+
+
 class Recipe {
-  constructor(name, ingredients, ingredientsQuantity, cookingTime, steps) {
+  constructor(name, ingredients, cookingTime, steps) {
     this.name = name;
     this.ingredients = ingredients;
-    this.ingredientsQuantity = ingredientsQuantity;
     this.cookingTime = cookingTime;
     this.steps = steps;
   }
@@ -49,20 +136,9 @@ class Recipe {
 
 let couscous = new Recipe(
   "Couscous",
-  ["Semoule", "viande", "Légumes"],
-  [250, 85, 100],
+  ["250g de semoule", " 85g de viande", "100g de légumes"],
   120,
   ["Mettre la semoule", "Mettre la viande", "Mettre les légumes"]
 );
 
 console.log(couscous);
-
-// Assigner le resultat de la recette dans une variable
-// let savedRecipe = handleFormSubmit();
-
-// Faire un truc avec des arrays pour les ingrédients et les quantités, et les associer
-function ListeDIngredients () {
-  let ingredients = [];
-  ingredients.push(document.getElementById("champ-liste-des-ingredients".value));
-  console.log(ingredients);
-};
